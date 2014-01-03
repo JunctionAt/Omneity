a87fbad6b0c645061a0995019d92bd837e6e51c4
Brandon Whaley <redkrieg@gmail.com>
2014-01-02 23:23:35 -0500

    add message when giving an item

--- actionsigns/give_sign.py
+++ actionsigns/give_sign.py
@@ -11,8 +11,9 @@ class GiveSign(SignBase):
     - location: [x, y, z]
       type: give
       give_item: 2
-      give_item_data: 0     [default: 0]
-      give_item_amount: 1   [default: 1]
+      give_item_data: 0            [default: 0]
+      give_item_amount: 1          [default: 1]
+      give_item_desc: 64 Diamonds  [default: an item]
     """
 
     def onLeftClick(self, sign_data, plugin, event, sign):
@@ -25,8 +26,11 @@ class GiveSign(SignBase):
         give_item = sign_data["give_item"]
         give_item_data = sign_data.get("give_item_data", 0)
         give_item_amount = sign_data.get("give_item_amount", 1)
+        give_item_desc = sign_data.get("give_item_desc", "an item")
 
-        if not self.safe_give(player, ItemStack(give_item, give_item_amount, give_item_data)):
+        if self.safe_give(player, ItemStack(give_item, give_item_amount, give_item_data)):
+            self.message(player, "You've been given %s!" % give_item_description)
+        else:
             self.message(player, "Please clean up your inventory a bit, there is no space :(")
 
         player.updateInventory()
