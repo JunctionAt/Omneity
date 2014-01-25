34a4c8f4d4971df5c0cce5d8a2b476323ec0a028
williammck <wjmck@me.com>
2014-01-25 01:55:33 +0000

    changed noportals a bit more - radius

--- noportals/__init__.py
+++ noportals/__init__.py
@@ -19,7 +19,22 @@ class PortalListener(BaseListener):
 
             #Check if the player near has the permission to light, if so light it.
             for player in Bukkit.getServer().getOnlinePlayers():
-                if (player.getLocation().distanceSquared(event.getBlocks()[0].getLocation()) < 25) and player.hasPermission("omneity.portal.light"):
+                if player.hasPermission("omneity.portal.light"):
+                    distance = player.getLocation().distanceSquared(event.getBlocks()[0].getLocation())
+                    if distance > 5:
+                        event.setCancelled(True)
+                        player.sendMessage("-")
+                        player.sendMessage(chatcolor.YELLOW + "You, or someone quite near, tried to light a portal.")
+                        player.sendMessage(chatcolor.GREEN + "You were able to light that portal!")
+                        player.sendMessage(chatcolor.RED + "If it was you that tried to light the portal,")
+                        player.sendMessage(chatcolor.RED + "stand inside the portal and light it.")
+                        player.sendMessage(chatcolor.GRAY + "This is very finicky - you might have to move up or sideways,")
+                        player.sendMessage(chatcolor.GRAY + "especially for really large portals.")
+                        player.sendMessage(chatcolor.GRAY + str(distance) + " square meters off - need to be under 5.")
+                        player.sendMessage(chatcolor.YELLOW + "Otherwise, ask them to request your assistance in lighting it.")
+                        player.sendMessage("-")
+                        return
+                    player.sendMessage(chatcolor.GREEN + "Portal successfully lit.")
                     return
 
             #Not able to light - continue on.
@@ -27,8 +42,11 @@ class PortalListener(BaseListener):
 
             for player in Bukkit.getServer().getOnlinePlayers():
                 if player.getLocation().distanceSquared(event.getBlocks()[0].getLocation()) < 25:
+                    player.sendMessage("-")
                     player.sendMessage(chatcolor.YELLOW + "You, or someone quite near, tried to light a portal.")
                     player.sendMessage(chatcolor.RED + "You'll have to modreq for one, as lighting your own is disabled.")
+                    player.sendMessage("-")
+                    return
 
 
 listener = PortalListener
